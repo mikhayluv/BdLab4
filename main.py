3b91fe6867fada4e77966b2e610c67d5fc1132b0
David <mikhaylov.david12@gmail.com>
2022-12-29 02:08:51 +0300

    update 1.4

--- main.py
+++ main.py
@@ -49,6 +49,18 @@ def create_planets():
     return planet_id
 
 
+def spaceship_on_planet(spaceship_id, planet_id):
+    string = "INSERT INTO s311289.Spaceship_on_planet (spaceship_id, planet_id) VALUES "
+    for index, value in enumerate(spaceship_id):
+        substring = "('" + str(value) + "', '" + str(random.choice(planet_id)) + "')"
+        if index != len(spaceship_id):
+            substring += ","
+        string += substring
+    string += ";"
+    stream = open("inserts/spaceship_on_planet.txt", "w")
+    stream.write(string)
+
+
 # Инсерты роботов
 def robots():
     string = "INSERT INTO s311289.Robots (model) VALUES "
@@ -70,6 +82,28 @@ def robots():
     stream = open("inserts/robot.txt", 'w')
     stream.write(string)
     return eve_robots_ids, robots_id
+
+
+def check_planet(eve_robots_ids, planet_id):
+    string = "INSERT INTO s311289.Check_planet (planet_id, is_habitable, check_date, robot_id) VALUES "
+    check_raznica = [3, 6]
+    max_check_date = datetime.date(year=2022, month=11, day=10)
+    check_of_planet = 0
+    for index, value in enumerate(planet_id):
+        temp_date = random_date(datetime.date(year=2015, month=1, day=1), datetime.date(year=2015, month=1, day=31))
+        while temp_date <= max_check_date:
+            check_of_planet += 1
+            next_date = temp_date + relativedelta(months=random.choice(check_raznica))
+            substring = "('" + str(value) + "', '" + "FALSE" + "','" + str(next_date) + "', '" + str(
+                random.choice(eve_robots_ids)) + "')"
+            temp_date = next_date
+            if index != len(planet_id) - 1 or temp_date <= max_check_date:
+                substring += ", "
+            string += substring
+    string += ";"
+    stream = open("inserts/check_planet.txt", 'w')
+    stream.write(string)
+    return check_of_planet
 
 
 # Инсерты людей
@@ -100,7 +134,8 @@ def humans():
 
 
 # Инсерты рабочих контрактов
-def work_contract(worker_ids, human_ids):  # Капитана можно присвоить одному человеку, id которого я выберу сам
+# Связать с посадкой людей. НУЖНЫ АЙДИ ЛЮДЕЙ, МЭЙСОН
+def work_contract(worker_ids):
     string = "INSERT INTO s311289.Work_contract (human_id, job_post, start_date, end_date) VALUES "
     contract_duration = [1, 2, 3, 5]
     max_future_date = datetime.date(year=2030, month=12, day=12)
@@ -168,19 +203,19 @@ def locations():
 # Инсерты людей на корабль
 def boarded_humans(humans_id):
     string = "INSERT INTO s311289.Human_on_spaceship (human_id , spaceship_id, boarded_date) VALUES "
-    boarded_date_for_spaceship_1 = datetime.datetime(year=2020, month=10, day=random.randrange(20, 27),
+    boarded_date_for_spaceship_1 = datetime.datetime(year=2015, month=1, day=random.randrange(1, 6),
                                                      hour=random.randrange(0, 23), minute=random.randrange(0, 60),
                                                      second=random.randrange(0, 60))
-    boarded_date_for_spaceship_2 = datetime.datetime(year=2021, month=5, day=random.randrange(8, 15),
+    boarded_date_for_spaceship_2 = datetime.datetime(year=2015, month=1, day=random.randrange(7, 13),
                                                      hour=random.randrange(0, 23), minute=random.randrange(0, 60),
                                                      second=random.randrange(0, 60))
-    boarded_date_for_spaceship_3 = datetime.datetime(year=2020, month=6, day=random.randrange(12, 19),
+    boarded_date_for_spaceship_3 = datetime.datetime(year=2015, month=1, day=random.randrange(14, 20),
                                                      hour=random.randrange(0, 23), minute=random.randrange(0, 60),
                                                      second=random.randrange(0, 60))
-    boarded_date_for_spaceship_4 = datetime.datetime(year=2021, month=7, day=random.randrange(5, 12),
+    boarded_date_for_spaceship_4 = datetime.datetime(year=2015, month=1, day=random.randrange(21, 27),
                                                      hour=random.randrange(0, 23), minute=random.randrange(0, 60),
                                                      second=random.randrange(0, 60))
-    boarded_date_for_spaceship_5 = datetime.datetime(year=2021, month=3, day=random.randrange(1, 7),
+    boarded_date_for_spaceship_5 = datetime.datetime(year=2015, month=1, day=random.randrange(27, 30),
                                                      hour=random.randrange(0, 23), minute=random.randrange(0, 60),
                                                      second=random.randrange(0, 60))
     person_on_ship_1 = []
@@ -214,6 +249,29 @@ def boarded_humans(humans_id):
     stream = open("inserts/boarded_humans.txt", 'w')
     stream.write(string)
     return person_on_ship_1, person_on_ship_2, person_on_ship_3, person_on_ship_4, person_on_ship_5
+
+
+def robots_on_spaceship(robots_id, eve_robots_ids, spaceship_id):
+    string = "INSERT INTO s311289.Human_on_spaceship (robot_id , spaceship_id, delivered_on_board_time) VALUES"
+    for index, value in enumerate(robots_id):
+        substirng = "('" + str(value) + "', '" + str(random.choice(spaceship_id)) + "', '" + str(
+            datetime.datetime(year=2014, month=random.randrange(1, 12), day=random.randrange(1, 28),
+                              hour=random.randrange(0, 23), minute=random.randrange(0, 60),
+                              second=random.randrange(0, 60))) + "')"
+        if index != len(robots_id) - 1:
+            substirng += ","
+        string += substirng
+    for index, value in enumerate(eve_robots_ids):
+        substirng = "('" + str(value) + "', '" + str(random.choice(spaceship_id)) + "', '" + str(
+            datetime.datetime(year=2014, month=random.randrange(1, 12), day=random.randrange(1, 28),
+                              hour=random.randrange(0, 23), minute=random.randrange(0, 60),
+                              second=random.randrange(0, 60))) + "')"
+        if index != len(eve_robots_ids) - 1:
+            substirng += ","
+        string += substirng
+    string += ";"
+    stream = open("inserts/robots_on_spaceship.txt", "w")
+    stream.write(string)
 
 
 # ТУТ ВЕЗДЕ ENUMERATE МОЖНО ЮЗАТЬ
@@ -268,19 +326,75 @@ def employee(active_contracts_id):
     return employee_id
 
 
+# Да, это жёстко
+def human_task(employee_id):
+    human_tasks = open("raw_data/task_for_human", "r", encoding="utf-8").read().splitlines()
+    string = "INSERT INTO s311289.Human_task (task, is_done, employee_id) VALUES "
+    task_id = []
+    done_task_id = []
+    who_did_it = []
+    for i in range(1, 10000):
+        task_id.append(i)
+        bol = ["TRUE", "FALSE"]
+        rnd_bool = random.choice(bol)
+        empl = random.choice(employee_id)
+        substring = "('" + random.choice(human_tasks) + "', '" + str(rnd_bool) + "', '" + str(empl) + "')"
+        if rnd_bool == "TRUE":
+            done_task_id.append(i)
+            who_did_it.append(empl)
+        if i != 12131:
+            substring += ","
+        string += substring
+    string += ";"
+    stream = open("inserts/human_task.txt", "w")
+    stream.write(string)
+    done_task_info = dict(zip(done_task_id, who_did_it))
+    return task_id, done_task_info
+
+
+def human_job_is_done(done_task_info):
+    string = "INSERT INTO s311289.Human_job_is_done (employee_id , task_id , task_start_time, task_end_time ) VALUES "
+    start = datetime.datetime(year=random.randrange(2020, 2022), month=random.randrange(1, 7),
+                              day=random.randrange(1, 23), hour=random.randrange(0, 18), minute=random.randrange(0, 60),
+                              second=random.randrange(0, 60))
+    i = 0
+    for key, value in done_task_info.items():
+        i += 1
+        finish = start + relativedelta(months=random.randrange(1, 5), day=random.randrange(1, 5),
+                                       hour=random.randrange(1, 5))
+        substring = "('" + str(key) + "', '" + str(value) + "', '" + str(start) + "', ' " + str(finish) + "')"
+        if i != len(done_task_info):
+            substring += ","
+        string += substring
+    string += ";"
+    stream = open("inserts/human_job_is_done.txt", "w")
+    stream.write(string)
+    return i
+
+
+# Разобраться, как привязать сюда людей на кораблях
+def human_task_location(location_with_spaceship_1, location_with_spaceship_2, location_with_spaceship_3,
+                        location_with_spaceship_4, location_with_spaceship_5):
+    string = "INSERT INTO s311289.Human_task_location (task_id, location_id) VALUES "
+
+
 def main():
     spaceship_id = spaceships()
     print(f"Id планет кораблей {spaceship_id}")
     planet_id = create_planets()
     print(f"Id планет {planet_id}")
+    spaceship_on_planet(spaceship_id, planet_id)
     eve_robots_ids, robots_id = robots()
     print(f"Id ЕВА {eve_robots_ids}")
     print(f"Id роботов {robots_id}")
+    robots_on_spaceship(robots_id, eve_robots_ids, spaceship_id)
+    check_of_planet = check_planet(eve_robots_ids, planet_id)
+    print(f"Кол-во чеков {check_of_planet}")
     human_amount, worker_ids, human_ids = humans()
     print(f"Все людей: {human_amount}")
     print(f"ID людей: {human_ids}")
     print(f"Количество работников: {len(worker_ids)}")
-    amount_of_contracts, active_contracts_id = work_contract(worker_ids, human_ids)
+    amount_of_contracts, active_contracts_id = work_contract(worker_ids)
     print(f"Количество контрактов: {amount_of_contracts}")
     print(f"Id активных контрактов: {active_contracts_id}")
     location_with_spaceship_1, location_with_spaceship_2, location_with_spaceship_3, location_with_spaceship_4, location_with_spaceship_5 = locations()
@@ -292,7 +406,11 @@ def main():
                    location_with_spaceship_1, location_with_spaceship_2, location_with_spaceship_3,
                    location_with_spaceship_4, location_with_spaceship_5)
     employee_id = employee(active_contracts_id)
-    print(f"ID работника {employee_id}")
+    print(f"ID работника: {employee_id}")
+    task_id, done_task_info = human_task(employee_id)
+    print(f"Оно? {done_task_info}")
+    i = human_job_is_done(done_task_info)
+    print(f"Кол-во выполненных тасков: {i}")
 
 
 main()
